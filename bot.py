65b52e74eb58eebd001fb6e86a2180125efa4015
darik2281337 <darik2281337@mail.ru>
2025-11-11 20:35:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
 import asyncio
-from pocketoptionapi_async.client import client  
+from pocketoptionapi_async.client import AsyncPocketOptionClient
+from pocketoptionapi_async.exceptions import ConnectionError
 
 SSID = '42["auth",{"sessionToken":"40527f1167b31e5240fc7c2b174589ce","uid":"115265714","lang":"ru","currentUrl":"cabinet/demo-quick-high-low","isChart":1}]'
 
-c = client(SSID, demo=True) 
+client = AsyncPocketOptionClient(SSID, demo=True)  
 
 async def main():
-   
-    await c.connect()
-   
-    balance = await c.get_balance()
-    print("Баланс:", balance)
-
+    try:
+        await client.connect()               
+        balance = await client.get_balance() 
+        print("Баланс:", balance)
+    except ConnectionError as e:
+        print("Ошибка подключения:", e)
 
 asyncio.run(main())
+
+
 
 
