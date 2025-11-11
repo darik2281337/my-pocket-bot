c594353a8c9ef2616e7591da009a45dd276f239c
darik2281337 <darik2281337@mail.ru>
2025-11-11 20:29:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
-from pocketoptionapi_async.client import Client
 import asyncio
+from pocketoptionapi_async.client import client  
 
-ssid = '42["auth",{"sessionToken":"40527f1167b31e5240fc7c2b174589ce","uid":"115265714","lang":"ru","currentUrl":"cabinet/demo-quick-high-low","isChart":1}]	'
+SSID = '42["auth",{"sessionToken":"40527f1167b31e5240fc7c2b174589ce","uid":"115265714","lang":"ru","currentUrl":"cabinet/demo-quick-high-low","isChart":1}]'
 
-client = Client(ssid=ssid, demo=True)
+c = client(SSID, demo=True) 
 
 async def main():
-    await client.connect()
-    balance = await client.get_balance()
+   
+    await c.connect()
+   
+    balance = await c.get_balance()
     print("Баланс:", balance)
 
+
 asyncio.run(main())
+
 
