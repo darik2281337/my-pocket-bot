d58488828e4f51c8b47a1e6cf8c0cddcd1ae7e2a
darik2281337 <darik2281337@mail.ru>
2025-11-11 20:08:18 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+from pocketoptionapi_async import Client
+import asyncio
+
+ssid = '42["auth",{"sessionToken":"40527f1167b31e5240fc7c2b174589ce","uid":"115265714","lang":"ru","currentUrl":"cabinet/demo-quick-high-low","isChart":1}]	'
+
+client = Client(ssid=ssid, demo=True)
+
+async def main():
+    await client.connect()
+    balance = await client.get_balance()
+    print("Баланс:", balance)
+
+asyncio.run(main())
