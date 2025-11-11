2653cced486bb2fd9ffdce4b382258aa0d8618e0
darik2281337 <darik2281337@mail.ru>
2025-11-11 20:20:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from pocketoptionapi_async import Client
+from pocketoptionapi_async.client import Client
 import asyncio
 
 ssid = '42["auth",{"sessionToken":"40527f1167b31e5240fc7c2b174589ce","uid":"115265714","lang":"ru","currentUrl":"cabinet/demo-quick-high-low","isChart":1}]	'
@@ -11,3 +11,4 @@ async def main():
     print("Баланс:", balance)
 
 asyncio.run(main())
+
